4255a670e2d438ea2564f2f04825ffc6f023e1c7
zcecil <zcecil@c59e6bd2-3bcf-0310-b476-95f61f225d3d>
2003-12-25 05:51:42 +0000

    blah

--- mod/summarize.pl
+++ mod/summarize.pl
@@ -10,13 +10,13 @@ use Encode::Guess;
 use Lingua::ZH::Numbers 'big5';
 use Lingua::ZH::Summarize ;
 
-# $Id$
-
 my $s = $MSG{body};
 my $reply;
 my $priority = 0;
 my $to       = $MSG{from};
 my $c;
+my $sumlen = 75;
+my $public = 1;
 if($s =~ /^summarize\s+(http:\S+)\s*/ && $MSG{to} eq $BOT_NICK) {
 ## Contributed by kcwu
   require LWP::UserAgent;
@@ -25,6 +25,7 @@ if($s =~ /^summarize\s+(http:\S+)\s*/ && $MSG{to} eq $BOT_NICK) {
 			       keep_alive => 1,
 			       timeout => 30,
 			      );
+	$ua->proxy('http','http://192.168.10.201:3128/');
   my @urlmatch=(		[
 		  'www.ettoday.com',
 		  sub {($title) = ($_[0]=~m/<td height="25"><font size="4" color="#0000ff"><b>([^<]*)<\/b>/im); $_[0]=~m/<font color="#4B4B4B" class="text">(.+)<\/font>/im},
@@ -32,7 +33,7 @@ if($s =~ /^summarize\s+(http:\S+)\s*/ && $MSG{to} eq $BOT_NICK) {
 		  sub { $_[0] =~ s/<[^<]+>//mg ; $_[0]},
 		  sub { $_[0] =~ s/\n//mg; $_[0] },
 		  sub { trim_whitespace(@_) ; @_ },
-		  sub { "東森新聞報 - [ ".$title." ]： ".Lingua::ZH::Summarize::summarize($_[0],maxlength => 70 )." " }
+		  sub { "東森新聞報 - [ ".$title." ]： ".Lingua::ZH::Summarize::summarize($_[0],maxlength => $sumlen )." " }
 		  #sub { "東森新聞報：[ ".$_[0]." ]" }
 		],
 		[
@@ -40,9 +41,121 @@ if($s =~ /^summarize\s+(http:\S+)\s*/ && $MSG{to} eq $BOT_NICK) {
 		   sub { ($title) = ($_[0]=~m/<font face="標楷體" size="5" color="#0000cc">\n([^<]*)\n\s*<\/font>/is); $_[0] =~ m/<!--== article begin ==-->(.+)<!--== article end ==-->/is },
 		   sub { $_[0] =~ s/<script.+<\/script>//mg ; $_[0]},
 		   sub { $_[0] =~ s/<[^<]+>//mg ; $_[0]},
+		   sub { $_[0] =~ s/\n//mg; $_[0] },
+		   sub { trim_whitespace(@_) ; @_ },
+		   sub { "[ ".$title." ]： ".Lingua::ZH::Summarize::summarize($_[0],maxlength => $sumlen )." " }
+		],
+		[
+		 'taiwan.cnet.com' ,
+		 sub { ($title) = ($_[0]=~m/<title>([^<]*)<\/title>/im); $title = (split(/[:]/,$title))[-1]; $_[0]=~ m/<!-- story body start -->(.+)<!-- story body end -->/is },
+		   sub { $_[0] =~ s/<script.+<\/script>//mg ; $_[0]},
+		   sub { $_[0] =~ s/<[^<]+>//mg ; $_[0]},
 		   #sub { $_[0] =~ s/\n//mg; $_[0] },
 		   sub { trim_whitespace(@_) ; @_ },
-		   sub { "[ ".$title." ]： ".Lingua::ZH::Summarize::summarize($_[0],maxlength => 70 )." " }
+		 sub { "CNET：[ ".$title." ]:".Lingua::ZH::Summarize::summarize($_[0],maxlength => $sumlen )." " }
+		],
+		  [
+		  'http://',
+#		   sub { ($_[0]=~m/[：:|]/ and (split /[：:|]/,$_[0])[-1]) or () },
+		   sub { $MSG{from} . ": 本網站尚無摘要功\能。" },
+		]
+	       );
+  
+  my $request = HTTP::Request->new('GET', "$1");
+  $ua->max_size(40960);
+  my $response;
+  eval {
+    local $SIG{ALRM} = sub { die"timeout\n"};
+    if($s =~ /udn/i) {
+	# $request = HTTP::Request->new('GET', "http://gugod.org/");
+	#$request = HTTP::Request->new('GET', 'file:///tmp/antiudn');
+	alarm 5;
+	$response = $ua->request($request);
+	alarm 0;
+    } else {
+	alarm 5;
+	$response = $ua->request($request);
+	alarm 0;
+	   }
+  };
+  if($@) {
+     die unless $@ eq "timeout\n";
+  }
+  unless($response->is_success) {
+    if($MSG{to} eq $BOT_NICK) {
+      $reply = "this url is broken";
+    }
+  } else {
+    my @content=();
+    foreach (@urlmatch) {
+      my $k = @$_[0];
+      my @v = @$_[1 .. (@$_-1)];
+      next unless $s =~ /\Q$k/m;
+      @content=($response->content);
+      foreach(@v) {
+	@content=$_->(@content);
+	last unless @content;
+      }
+      last;
+    }
+    if(defined $content[0]) {
+      $reply=$content[0];
+      $to="";
+
+      # Decode special characters 
+      $reply =~ s/&#(\d+);/pack('U*', $1)/eg;
+      $reply =~ s/&gt;/>/g;
+      $reply =~ s/&lt;/</g;
+      $reply =~ s/&amp;/&/g;
+
+      foreach(qw/big5 big5-eten shiftjis gb2312-raw gb12345-raw hz iso-ir-165 cp936 utf8/) {
+        my $decoder = guess_encoding($reply, ($_));
+        if(ref($decoder)) {
+	  my $utf8 = $decoder->decode($reply);
+	  $reply = Encode::encode("big5", $utf8);
+          last;
+        }
+      }
+
+    }
+  }
+}elsif($s =~ /^content\s+(http:\S+)\s*/ && $MSG{to} eq $BOT_NICK) {
+## Contributed by kcwu
+  require LWP::UserAgent;
+  my $title="";
+  $public = 0;
+  my $ua = LWP::UserAgent->new(env_proxy => 1,
+			       keep_alive => 1,
+			       timeout => 30,
+			      );
+	$ua->proxy('http','http://192.168.10.201:3128/');
+  my @urlmatch=(		[
+		  'www.ettoday.com',
+		  sub {($title) = ($_[0]=~m/<td height="25"><font size="4" color="#0000ff"><b>([^<]*)<\/b>/im); $_[0]=~m/<font color="#4B4B4B" class="text">(.+)<\/font>/im},
+		  sub { $_[0] =~ s/<script.+<\/script>//mg ; $_[0]},
+		  sub { $_[0] =~ s/<[^<]+>//mg ; $_[0]},
+		  sub { $_[0] =~ s/\n//mg; $_[0] },
+		  sub { trim_whitespace(@_) ; @_ },
+		  sub { "東森新聞報 - [ ".$title." ]： ".$_[0]." " }
+		  #sub { "東森新聞報：[ ".$_[0]." ]" }
+		],
+		[
+			'tw.news.yahoo.com',
+		   sub { ($title) = ($_[0]=~m/<font face="標楷體" size="5" color="#0000cc">\n([^<]*)\n\s*<\/font>/is); $_[0] =~ m/<!--== article begin ==-->(.+)<!--== article end ==-->/is },
+		   sub { $_[0] =~ s/<script.+<\/script>//mg ; $_[0]},
+		   sub { $_[0] =~ s/<[^<]+>//mg ; $_[0]},
+		   sub { $_[0] =~ s/\n//mg; $_[0] },
+		   sub { trim_whitespace(@_) ; @_ },
+		   sub { "[ ".$title." ]： ".$_[0]." " }
+		],
+		[
+		 'taiwan.cnet.com' ,
+		 sub { ($title) = ($_[0]=~m/<title>([^<]*)<\/title>/im); $title = (split(/[:]/,$title))[-1]; $_[0]=~ m/<!-- story body start -->(.+)<!-- story body end -->/is },
+		   sub { $_[0] =~ s/<script.+<\/script>//mg ; $_[0]},
+		   sub { $_[0] =~ s/<[^<]+>//mg ; $_[0]},
+		   #sub { $_[0] =~ s/\n//mg; $_[0] },
+		   sub { trim_whitespace(@_) ; @_ },
+		 sub { "CNET：[ ".$title." ]:".$_[0]." " }
 		],
 		  [
 		  'http://',
@@ -116,6 +229,7 @@ if($s =~ /^summarize\s+(http:\S+)\s*/ && $MSG{to} eq $BOT_NICK) {
 			       keep_alive => 1,
 			       timeout => 30,
 			      );
+	$ua->proxy('http','http://192.168.10.201:3128/');
   my @urlmatch=([
 		 'udn' ,
 #		 sub { "為了對聯合報系的抵制不列出標題" },
@@ -256,7 +370,7 @@ my %rmsg = (
     priority => $priority,
     from     => $BOT_NICK ,
     to       => $to,
-    public   => 1,
+    public   => $public,
     body     => $reply
     );
 
