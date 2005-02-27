c01c471eb4decd55503b6941c849bc1614290fab
gugod <gugod@c59e6bd2-3bcf-0310-b476-95f61f225d3d>
2005-02-27 17:31:03 +0000

    r11368@Liu-Kang-min:  gugod | 2005-02-28 00:15:41 +0800  .

--- lib/Jabbot/Console.pm
+++ lib/Jabbot/Console.pm
@@ -1,6 +1,11 @@
 package Jabbot::Console;
 use Jabbot::Base -Base;
+use Term::ReadLine;
 
 sub process {
-
+    my $term = new Term::ReadLine 'Jabbot-Console';
+    while(defined($_ = $term->readline('jabbot> '))){
+        my $reply = $self->hub->process($_);
+        print $term->OUT "$reply \n";
+    }
 }
