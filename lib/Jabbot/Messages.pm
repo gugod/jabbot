0a3fd26a406cf0337471ca36919f1f6077675df8
gugod <gugod@c59e6bd2-3bcf-0310-b476-95f61f225d3d>
2005-02-28 10:20:09 +0000

    r11434@Liu-Kang-min:  gugod | 2005-02-28 18:01:22 +0800  Message queue object

---
+++ lib/Jabbot/Messages.pm
@@ -0,0 +1,18 @@
+package Jabbot::Messages;
+use Jabbot::Base -Base;
+
+field must_say => [];
+field normal => [];
+
+sub append {
+    my $msg = shift; # a Jabbot::Message obj
+    if($msg->must_say) {
+        push @{$self->must_say},$msg;
+    } else {
+        push @{$self->normal},$msg;
+    }
+}
+
+sub next {
+    shift(@{$self->must_say}) || shift(@{$self->normal})
+}
