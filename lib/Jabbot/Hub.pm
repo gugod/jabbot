f3cd798eacca554ea0b787828b0a80cfa4418ea6
gugod <gugod@c59e6bd2-3bcf-0310-b476-95f61f225d3d>
2005-03-19 19:30:57 +0000

    r14976@kybristedi:  gugod | 2005-03-20 03:30:36 +0800  more strict on checking wheter if it is a message object

--- lib/Jabbot/Hub.pm
+++ lib/Jabbot/Hub.pm
@@ -1,24 +1,30 @@
 package Jabbot::Hub;
 use Spoon::Hub -Base;
 use List::Util qw(shuffle);
+use UNIVERSAL qw(isa can);
 
 sub init {
     $self->use_class('messages');
     $self->use_class('message');
 }
 
+use YAML;
 sub process {
     $self->preload;
     my $msg = shift;
     my @replies = grep {
         defined $_->text && $_->text ne '' && !($_->text =~ /^\s+$/)
+    } grep {
+        defined $_ && isa($_,'Jabbot::Message')
     } map {
-        eval { $self->$_->process($msg) } or $self->message->new;
+        my $reply;
+        eval {$reply = $self->$_->process($msg);};
+        $@ ? $self->message->new : $reply;
     } $self->all_plugin_ids;
     if(my @musts = grep {$_->must_say} @replies) {
         $self->messages->append($_) for @musts;
     } else {
-        $self->messages->append((shuffle @replies)[0]);
+        $self->messages->append((shuffle @replies)[0] || $self->message->new);
     }
     $self->messages->next;
 }
