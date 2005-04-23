6e0c9a09664745e536f623b9903b28ef62ff19c0
gugod <gugod@c59e6bd2-3bcf-0310-b476-95f61f225d3d>
2005-04-23 23:26:28 +0000

    r17262@gugod:  gugod | 2005-04-24 07:22:55 +0800  refine the 'seen' command reply in a purl way.

--- lib/Jabbot/Seen.pm
+++ lib/Jabbot/Seen.pm
@@ -1,5 +1,6 @@
 package Jabbot::Seen;
 use Jabbot::Plugin -Base;
+use Time::Interval;
 
 const class_id => 'seen';
 
@@ -7,14 +8,24 @@ sub process {
     my $msg = shift;
     my $nick = $msg->from;
     my $db = io->catfile($self->plugin_directory,"seen.db")->assert;
-    my $now = time;
     my $reply;
     if($msg->text =~ /^seen\s+([^\s\?]+)\s*\?*/) {
-	my $t = gmtime($db->{$1}) . " GMT";
-        $reply = defined($db->{$1})?
-            "$1 was seen on $t":
-            "I havn't seen $1 , $nick";
+	if(defined($db->{$1})) {
+	    my $t1 = gmtime($db->{$1}) . " GMT";
+	    my $t2 = gmtime(time) . " GMT";
+	    my $interval = getInterval($t1,$t2);
+	    $reply = "$1 was seen ";
+	    for(qw(days hours minutes seconds)) {
+		if($interval->{$_}) {
+		    $reply .= "$interval->{$_} $_ ";
+		}
+	    }
+	    $reply .= 'ago';
+	} else {
+	    $reply = "I havn't seen $1 , $nick";
+	}
     }
     $db->{$nick} = time;
     $self->reply($reply,1);
 }
+
