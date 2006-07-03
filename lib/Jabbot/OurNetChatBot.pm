86c18c44a2cfe9d4849194ccfe9b9080814dd682
gugod <gugod@c59e6bd2-3bcf-0310-b476-95f61f225d3d>
2006-07-03 18:19:57 +0000

    r246@irc:  gugod | 2005-11-13 10:31:20 +0800  Reconstruct the chatbot object everytime.  The bot doesn't seem to reply after it's been constructed for a long time.

--- lib/Jabbot/OurNetChatBot.pm
+++ lib/Jabbot/OurNetChatBot.pm
@@ -5,18 +5,19 @@ use Encode;
 
 const class_id => 'ournet_chatbot';
 
-field chatbot => {},
-    -init => q{new OurNet::ChatBot($self->config->{nick},$self->chatdb,0)};
+# field chatbot => {},
+#     -init => q{new OurNet::ChatBot($self->config->{nick},$self->chatdb,0)};
 
 sub chatdb {
     io->catfile($self->plugin_directory,$self->config->{nick})->name
 }
 
-use YAML;
 sub process {
     my $s = shift->text;
     my $big5_s = Encode::encode('big5',$s);
-    my $reply = $self->chatbot->input($big5_s);
+    my $chatbot = OurNet::ChatBot->new($self->config->{nick},$self->chatdb,0);
+    my $reply = $chatbot->input($big5_s);
     my $utf8_reply = Encode::decode('big5',$reply);
     $self->reply($utf8_reply);
 }
+
