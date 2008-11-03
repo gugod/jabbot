3122d78c119e09bde0560b80d5112e5b7c2591da
Liu Kang-min <gugod@gugod.org>
2008-11-03 18:18:15 +0800

    A "Facts" plugin that acts like infobot.
    Not fully implement exactuly the way infobot accepts, but will be.

---
+++ lib/Jabbot/Facts.pm
@@ -0,0 +1,62 @@
+package Jabbot::Facts;
+use Jabbot::Plugin -Base;
+
+const class_id => 'facts';
+
+field db => {}, -init => q{$self->_load};
+
+sub process {
+    my $text = shift->text;
+    my $r = $self->react_to($text);
+    $self->reply( $r );
+}
+
+my %D2P = (
+    qr/(.+?)\s+is\s+(.+)/i        => \&_save,
+    qr/no,\s*(.+?)\s+is\s+(.+)/i  => \&_reset,
+    qr/forget\s+(.+)/i            => \&_forget,
+    qr/(.{1,64})\s*\?+/           => \&_query,
+);
+
+sub react_to {
+    my $text = shift;
+
+    while(my ($regex, $sub) = each(%D2P)) {
+        next unless $text =~ m/$regex/;
+        return $sub->($self, $1, $2);
+    }
+}
+
+sub _save {
+    my ($X, $Y) = @_;
+    $self->db->{$X} = $Y;
+    "ok";
+}
+
+sub _reset {
+    my ($X, $Y) = @_;
+    $self->db->{$X} = $Y;
+    "ok!";
+}
+
+sub _forget {
+    my ($X) = @_;
+    delete $self->db->{$X};
+    "What is $X ?";
+}
+
+sub _query {
+    my ($X) = @_;
+    return "" unless defined(my $r =  $self->db->{$X});
+    if ($r =~ s/^<reply>\s*//) {
+        return $r;
+    }
+    return "It is $r";
+}
+
+sub _load {
+    io($self->plugin_directory)->mkpath;
+    my $db = io->catfile($self->plugin_directory,'main.db')->utf8->assert;
+    $self->db($db);
+    return $db;
+}
