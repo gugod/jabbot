c9867a1068937a6fabec2d05a0454f09f67a6cea
Liu Kang-min <gugod@gugod.org>
2008-11-29 15:40:59 +0800

    A Math plugin that evals simple math expression.

---
+++ lib/Jabbot/Math.pm
@@ -0,0 +1,27 @@
+package Jabbot::Math;
+use Jabbot::Plugin -Base;
+use Math::Expression;
+
+field 'class_id' => "math";
+
+field 'pasred';
+
+sub test {
+    my $text = shift;
+    return $text =~ m{^[\s\d\+\-\*\/^]+$};
+}
+
+sub process {
+    my $msg = shift;
+    my $text = $msg->text;
+    $text =~ s/^\s+//; $text =~ s/\s$//;
+
+    return unless $self->test($text);
+
+    my $m = new Math::Expression;
+    my $tree = $m->Parse($msg->text);
+    return unless $tree;
+
+    my $reply = $m->EvalToScalar($tree);
+    $self->reply($reply, 1) unless $reply eq $text;
+}
