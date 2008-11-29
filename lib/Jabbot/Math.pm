543dad6536704b1a4ffb1a8f487fa33bfa21e920
Liu Kang-min <gugod@gugod.org>
2008-11-29 16:01:22 +0800

    Normalize the input a little bit so now it accepts optional question marks at the end.

--- lib/Jabbot/Math.pm
+++ lib/Jabbot/Math.pm
@@ -14,12 +14,13 @@ sub test {
 sub process {
     my $msg = shift;
     my $text = $msg->text;
-    $text =~ s/^\s+//; $text =~ s/\s$//;
+
+    $text =~ s/^\s+//; $text =~ s/\s*\?*\s*$//;
 
     return unless $self->test($text);
 
     my $m = new Math::Expression;
-    my $tree = $m->Parse($msg->text);
+    my $tree = $m->Parse($text);
     return unless $tree;
 
     my $reply = $m->EvalToScalar($tree);
