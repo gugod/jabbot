f23cc947f61bd10cdaa43a4af63da5c5a4705aec
Liu Kang-min <gugod@gugod.org>
2009-04-27 00:18:07 +0800

    basically the whole IsaDB is now renamed to zh_tw/UrbanFacts
    It's under zh_tw because of it basically only accepts Chinese
    language. It's named with "Urban" because it'll simply take whatever
    said from anyone as facts.

--- lib/Jabbot/zh_tw/UrbanFacts.pm
+++ lib/Jabbot/zh_tw/UrbanFacts.pm
@@ -1,15 +1,15 @@
-package Jabbot::IsaDB;
+package Jabbot::zh_tw::UrbanFacts;
 use Jabbot::Plugin -Base;
 use utf8;
 
 my $ymodifiers = "好像|應該|就|也|乃|只|衹|真的|真";
 
-const class_id => 'isa_db';
+const class_id => 'zh_tw__urban_facts';
 field isadb => {}, -init => q{$self->load_isadb};
 
 sub load_isadb {
     io($self->plugin_directory)->mkpath;
-    my $db = io->catfile($self->plugin_directory,'isa.db')->utf8->assert;
+    my $db = io->catfile($self->plugin_directory ,'main.db')->utf8->assert;
     $self->isadb($db);
     return $db;
 }
@@ -21,7 +21,8 @@ sub process {
     return unless($msg->to eq $self->config->{nick});
     $qstring =~ s/^(.*)是誰\s*(\?|？)$/誰是$1？/;
 
-    if ($qstring =~ /^誰是/) {$qstring .= "？"}
+    if ($qstring =~ /^誰是/) { $qstring .= "？" }
+
     if($qstring =~ /(\?|？)$/ ) {
 	$qstring =~ s/(?:\?|？|\s)+$//;
 	if($msg->to eq $self->config->{nick}) {
@@ -40,37 +41,6 @@ sub process {
             $qstring =~ s/\s*是什麼//;
 	}
         $r = $self->isadb->{$qstring} if defined $self->isadb->{$qstring};
-    } elsif($qstring =~ /^(?:(?:(?:dump|(?:tell me)) all keywords about)|(?:what do you know about))\s+([^\?]+)[\s\?]*$/) {
-	my $wanted = quotemeta($1);
-	my @iknow = grep { $_ =~ /$wanted/i } keys %{$self->isadb};
-	if(@iknow) {
-	    $r = "一共有 " .( $#iknow + 1) ." 筆: " . join(" || ", @iknow);
-	    $r = "一共有 ". ($#iknow + 1) ." 筆, 實在太多了"
-		if (length($r) > 400);
-	}
-	$r ||= "我啥都不知道，別打我";
-    } elsif($qstring =~ /^(?:dump|(?:tell me)) all about\s+(.*)\s*$/) {
-	my $wanted = quotemeta($1);
-	my @iknow = grep { $_ =~ /$wanted/i } keys %{$self->isadb};
-	my $k = scalar(@iknow);
-	$k = 3 if $k > 3;
-	$r = join("。", map {$self->isadb->{$_}} (sort { rand() <=> rand() } @iknow)[0..$k]);
-	$r = "太多了，三天三夜說不完" if (length($r) > 400);
-	$r ||= "我啥都不知道，別打我";
-    } elsif($qstring =~ /^forget all about\s+(.*)\s*$/ && $msg->to eq $self->config->{nick}) {
-	my $wanted = quotemeta($1);
-	my $n = 0 ;
-	map { $n++; delete $self->isadb->{$_} }
-            grep { $_ =~ /$wanted/ }
-                keys %{$self->isadb};
-	$r= ($n == 0)? "並無任何與 $1 有關的資料"
-            : "一共有 $n 筆資料從資料庫中永遠刪除了";
-    } elsif($qstring =~ /(?:anything\s+about\s+)(.*)\s*(\?!!!+)/ && $msg->to eq $self->config->{nick}) {
-	my $wanted = quotemeta($1);
-	$r = $self->rand_choose(map {$self->isadb->{$_}} grep { $_ =~ /$wanted/ } keys %{$self->isadb});
-    } elsif($qstring =~ /^forget\s+(.*)$/  && $msg->to eq $self->config->{nick}) {
-	delete $self->isadb->{$1};
-	$r= "ok";
     } else {
         eval { $r = $self->do_my_job($qstring,$msg) }
             or print $@;
@@ -98,20 +68,6 @@ sub do_my_job {
                 $db->{$k} = $_;
                 $r = $self->rand_choose(@rdb);
             }
-        } elsif (/(.+)\sis\salso\s(.+)/i) {
-            my ($k,$v) = ($1,$2);
-            $k =~ s/\s+$//;
-            if(length($db->{$k}) > 0) {
-                $db->{$k} = $v;
-            } else {
-                $db->{$k} .= " or $v";
-            }
-            $r = $self->rand_choose(@rdb);
-        } elsif (/\s[Ii][Ss]\s/) {
-            my ($k,$v) = split(/\s[Ii][Ss]\s/ , $_, 2);
-            $k =~ s/\s+$//;
-            $self->isadb->{$k} = $_;
-            $r = $self->rand_choose(@rdb);
         }
     }
     return $r;
