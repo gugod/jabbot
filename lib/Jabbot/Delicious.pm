b536d82ee024a345db7ab4df223e54f223867c58
Kang-min Liu <gugod@gugod.org>
2009-06-21 18:39:53 +0800

    auto delicious urls

--- lib/Jabbot/Delicious.pm
+++ lib/Jabbot/Delicious.pm
@@ -13,29 +13,13 @@ sub process {
 
     my $msg = shift;
     my $msg_from = $msg->from;
+
     if ($msg->text =~ /^(?:spread|\+d|delicious) +($RE{URI}{HTTP})(?: +tags:(.+?))?$/i) {
         my ($url,$tags) = ($1,$2);
-        my $del = Net::Delicious->new({
-            user => $config->{username},
-            pswd => $config->{password},
-            debug => 1
-        });
 
-        print "!! Posting $url to delicious\n";
+        $tags ||= "";
 
-        my $title = title($url) || "";
-        my $auto_tags = join " ", split /\W/, "$url  $title";
-
-        my $for_user = "";
-        if (defined($_ = $config->{user_mapping}{$msg_from})) {
-            $for_user = "for:$_";
-        }
-
-        my $res = $del->add_post({
-            url => $url,
-            tags => "$for_user $auto_tags by_${msg_from} " . ($tags||""),
-            description => $url,
-        });
+        my $res = $self->post_to_delicious($url, "$tags by_${msg_from}");
 
         if($res) {
             $self->reply("that's delicious, $msg_from", 1);
@@ -44,4 +28,21 @@ sub process {
             $self->reply("something wrong with delicious, $msg_from", 1);
         }
     }
+    elsif ($msg->text =~ /($RE{URI}{HTTP})/) {
+        $self->post_to_delicious($1);
+    }
+}
+
+sub post_to_delicious {
+    my ($url, $given_tags) = @_;
+    $given_tags ||= "";
+
+    my $config = $self->hub->config->{delicious};
+    my $del = Net::Delicious->new({ user => $config->{username}, pswd => $config->{password} });
+
+    my $title = title($url) || "";
+    my $auto_tags = join " ", split /\W/, "$url $title";
+
+    my $res = $del->add_post({ url => $url, tags => "$given_tags $auto_tags", description => $url });
+    return $res;
 }
