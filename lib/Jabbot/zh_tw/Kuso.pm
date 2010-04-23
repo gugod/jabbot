1f8bea15ebb14d533dd404a5ab6c8abd04de9226
Kang-min Liu <gugod@gugod.org>
2010-04-23 12:06:03 +0800

    becasue c9s is the man

--- lib/Jabbot/zh_tw/Kuso.pm
+++ lib/Jabbot/zh_tw/Kuso.pm
@@ -4,12 +4,20 @@ use utf8;
 
 const class_id => 'zh_tw_kuso';
 
+sub maybe() {
+    (rand > 0.75)
+}
+
 sub process {
     my $msg = shift;
     my $text = $msg->text;
 
     if ($text eq '!') {
-        return $self->reply("驚嘆號是棒槌", 1);
+        return $self->reply("驚嘆號是棒槌", maybe);
+    }
+
+    if ($text =~ /還不賴(!?)/) {
+        return $self->reply(($1?"驚嘆號是棒槌，":"") . "真的還不賴", maybe);
     }
 
     if ($msg->me) {
