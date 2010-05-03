53d6f9731b6c80c05c5e2c6806749a6db5925fd0
Kang-min Liu <gugod@gugod.org>
2010-05-04 07:50:27 +0800

    a console that use RemoteCore

---
+++ lib/Jabbot/Front/Console.pm
@@ -0,0 +1,22 @@
+package Jabbot::Front::Console;
+use common::sense;
+use Jabbot::RemoteCore;
+use Term::ReadLine;
+
+sub run {
+    say "Jabbot Console. Hit Ctrl-C to quit.";
+
+    my $j = Jabbot::RemoteCore->new;
+    my $term = Term::ReadLine->new('jabbot');
+    my $prompt = "\njabbot> ";
+    my $OUT = $term->OUT || \*STDOUT;
+    while ( defined ($_ = $term->readline($prompt)) ) {
+        my $ans = $j->answer(question => $_);
+        warn $@ if $@;
+        say $OUT $ans unless $@;
+
+        $term->addhistory($_) if /\S/;
+    }
+}
+
+&run;
