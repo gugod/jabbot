dc65a8b9897db55b99a67e2c6695b86c713fb371
Kang-min Liu <gugod@gugod.org>
2010-05-05 11:24:40 +0800

    Rewrite Jabbot::Plugin

--- lib/Jabbot/Plugin.pm
+++ lib/Jabbot/Plugin.pm
@@ -1,37 +1,13 @@
 package Jabbot::Plugin;
-use Spoon::Plugin -Base;
+use common::sense;
+use Object::Tiny;
 
-stub 'class_id';
-const config_file => '';
-const class_title_prefix => 'Jabbot';
-
-field config  => -init => '$self->hub->config';
-field message => -init => '$self->hub->message';
-
-sub new {
-    return $self if ref $self;
-    super;
+sub can_answer {
+    die "Plugin should implement 'can_answer($question)' method\n";
 }
 
-sub init {
-    $self->hub->config->add_file($self->config_file);
+sub answer {
+    die "Plugin should implement 'answer($question)' method\n";
 }
 
-sub reply {
-    my ($text,$must) = @_;
-    return $self->message->new(
-        text => $text,
-        (must_say => (defined $text) ? $must : undef)
-    );
-}
-
-sub trim {
-    for(@_) {
-        s/^\s+//;
-        s/\s+$//;
-    }
-}
-
-sub rand_choose {
-    $_[int(rand($#_+1))];
-}
+1;
