ed4aeef3dc31bc5c19d6902c0deb6b5ebac0376e
Kang-min Liu <gugod@gugod.org>
2010-05-16 00:53:52 +0800

    use the new Acme::Lingua::ZH::Remix api

--- lib/Jabbot/Plugin/zh_tw/RandomChatbot.pm
+++ lib/Jabbot/Plugin/zh_tw/RandomChatbot.pm
@@ -1,14 +1,18 @@
 package Jabbot::Plugin::zh_tw::RandomChatbot;
 use common::sense;
-use Acme::Lingua::ZH::Remix 0.14;
+use Object::Tiny;
+use Acme::Lingua::ZH::Remix 0.90;
+use self;
 
-sub new { bless {}, shift }
-
-sub can_answer { 1 }
+sub can_answer {
+    my ($text) = @args;
+    return (lenght($text) > 3);
+}
 
 sub answer {
+    my $remix = Acme::Lingua::ZH::Remix->new;
     return {
-        content    => rand_sentence,
+        content    => $remix->random_sentence,
         confidence => 0
     }
 }
