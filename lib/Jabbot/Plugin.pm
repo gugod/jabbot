8173678592f59c0c31a10e4f2a961a801d1b7ac5
Kang-min Liu <gugod@gugod.org>
2010-05-16 22:20:29 +0800

    correct syntax

--- lib/Jabbot/Plugin.pm
+++ lib/Jabbot/Plugin.pm
@@ -3,11 +3,11 @@ use common::sense;
 use Object::Tiny;
 
 sub can_answer {
-    die "Plugin should implement 'can_answer($question)' method\n";
+    die "Plugin must implement 'can_answer(\$question)' method\n";
 }
 
 sub answer {
-    die "Plugin should implement 'answer($question)' method\n";
+    die "Plugin must implement 'answer(\$question)' method\n";
 }
 
 1;
