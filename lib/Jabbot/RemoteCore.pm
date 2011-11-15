f0fa6c5531847263b45abc6279c49534731cdc23
Kang-min Liu <gugod@gugod.org>
2011-11-15 14:43:34 +0800

    rewrite RemoteCore

--- lib/Jabbot/RemoteCore.pm
+++ lib/Jabbot/RemoteCore.pm
@@ -1,57 +1,60 @@
 package Jabbot::RemoteCore;
 use common::sense;
-use Object::Tiny;
-use HTTP::Lite;
-use JSON qw(from_json);
-
 use AnyEvent;
 use AnyEvent::MP;
 use AnyEvent::MP::Global;
 
+my $self = bless {}, __PACKAGE__;
+
+sub new {
+    return $self;
+}
+
 configure;
 
-sub DESTROY {}
+grp_reg jabbot_remotecore => rcv
+    port,
+    reply => sub {
+        my $data = shift;
+        $self->{answers}->send(@{ $data->{answers} });
+    };
 
-our $AUTOLOAD;
+sub answers {
+    my ($self, %args) = @_;
+    my @answers;
+    my $q = $args{question};
+    return if $self->{answers};
 
-sub AUTOLOAD {
+    $self->{answers} = AE::cv;
+
+    my $ready;
+
+    $ready = AE::idle sub {
+        if (my $ports = grp_get "jabbot_core") {
+            undef $ready;
+
+            for (@$ports) {
+                snd $_, action => { node => "jabbot_remotecore", name => "answers", args => \%args };
+            }
+        }
+    };
+
+    my @answers = $self->{answers}->recv;
+
+    delete $self->{answers};
+    return [sort { $b->{confidence} <=> $a->{confidence} } @answers];
+}
+
+sub answer {
     my ($self, %args) = @_;
 
-    my $name = $AUTOLOAD;
-    $name =~ s/.*://;
-
-    print "XDXDXD $name %args\n";
-
-    my $ports = grp_get "jabbot_core" or return;
-
-    print "XDXDXD 123123\n";
-
-    for (@$ports) {
-        snd $_, action => { name => $name, args => \%args }
-    }
-
-    # my $core_server = "http://localhost:15000";
-    # my $http = HTTP::Lite->new;
-    # $http->prepare_post(\%args);
-    # my $status = $http->request("${core_server}/${name}");
-    # if ($status == 200) {
-    #     my $body = $http->body;
-    #     if ($body ne "OK") {
-    #         my $response = from_json($http->body);
-    #         return $response->{$name};
-    #     }
-    # }
-
-    return $self;
+    return $self->answers(%args)->[0];
 }
 
 1;
 
-=head1 SYNOPSIS
+__END__
 
-plackup -s Twiggy -l :15000 -Ilib -MJabbot::Core       -e '\&Jabbot::Core::app' &
-plackup -s Twiggy -l :15001 -Ilib -MJabbot::Front::IRC -e "\&Jabbot::Front::IRC::app" &
+A console:
 
-perl -Ilib -MJabbot::RemoteCore -E 'Jabbot::RemoteCore->post(channel => "/irc/networks/freenode/channels/jabbot", text => "OHAI 1337")'
-
-=cut
+perl -Ilib -MJabbot::RemoteCore -E 'while(<>) { say Jabbot::RemoteCore->new->answer(question => $_)->{content}; } AE::cv->recv'
