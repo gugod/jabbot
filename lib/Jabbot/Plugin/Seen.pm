d9383a33f9fdda2cbc1f866ea22e3d75cfe8759b
Kang-min Liu <gugod@gugod.org>
2011-11-16 22:16:31 +0800

    an "Seen" pluging (only logging at this moment)

---
+++ lib/Jabbot/Plugin/Seen.pm
@@ -0,0 +1,28 @@
+package Jabbot::Plugin::Seen;
+use Jabbot::Plugin;
+use Jabbot;
+use AnyEvent;
+
+sub can_answer {
+    my ($text, $message) = @args;
+
+    if ($text =~ /seen\s+(\W+)\s*\?$/) {
+        $self->{nick} = $1;
+        return 1;
+    }
+
+    Jabbot->memory->set("seen", $message->{from}, { time => time, message => $message });
+    return 0;
+}
+
+sub answer {
+    my ($text) = @args;
+    my $nick = $self->{nick};
+
+    # my $ans = AE::cv;
+    # Jabbot->memory->get("seen", $nick, $ans);
+
+    return $ans->recv;
+}
+
+1;
