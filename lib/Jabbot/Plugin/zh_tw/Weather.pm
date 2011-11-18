ee93a187ac8b8a2637bd1dcc6050cf50bb56cc65
Kang-min Liu <gugod@gugod.org>
2011-11-19 02:58:59 +0800

    report forecast, and add a specialized syntax

--- lib/Jabbot/Plugin/zh_tw/Weather.pm
+++ lib/Jabbot/Plugin/zh_tw/Weather.pm
@@ -5,8 +5,14 @@ use Weather::Google;
 sub can_answer {
     my ($text) = @args;
 
+    if ($text =~ /^\s*(.+)\s*天氣[?？]?\s*$/) {
+        $self->{area} = $1;
+        $self->{hint} = "天氣";
+        return 1;
+    }
+
     ($self->{area}) = $text =~ m/((?:南投|嘉義|基隆|宜蘭|屏東|彰化|新北|新竹|桃園|澎湖|臺中|臺北|臺南|臺東|台中|台北|台南|台東|花蓮|苗栗|連江|金門|雲林|高雄)(?:縣|市)?)/;
-    ($self->{hint}) = $text =~ m/(weather|冷|熱|雨|晴|天氣(?:預報)?)/;
+    ($self->{hint}) = $text =~ m/(weather|冷|暖|寒|暑|熱|涼|雨|晴|天氣|氣象)/;
     ($self->{forcast}) = $text =~ m/(預報)/;
 
     return $self->{area} && $self->{hint};
@@ -16,16 +22,19 @@ use YAML;
 sub answer {
     my ($text) = @args;
 
-    if ($self->{area} =~ /高雄/) {
-        $self->{area} = "高雄市"
-    }
-
     my $weather = Weather::Google->new($self->{area}, { language => 'zh-TW' });
     my $current = $weather->current_conditions;
 
-    my $reply = "不知道...";
+    my $reply;
 
-    if ($current->{condition}) {
+    if ($self->{forcast}) {
+        $reply = "";
+        my $days = $weather->forecast_conditions;
+        foreach my $day (@$days) {
+            $reply .= $day->{day_of_week} . ": " . $day->{condition} . ", " . $day->{low} . " ~ " . $day->{high} . " ℃。" ;
+        }
+    }
+    elsif ($current->{condition}) {
         $reply = join(
             ", ",
             "$self->{area} 現在 $current->{condition}",
@@ -34,6 +43,8 @@ sub answer {
             $current->{wind_condition}
         );
     }
+
+    $reply ||= "不知道...";
 
     return { content => $reply, confidence => 1 };
 }
