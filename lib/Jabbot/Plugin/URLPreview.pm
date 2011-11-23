dcb187cdeb95466978dbc0c79f4a8819217f4618
c9s <cornelius.howl@gmail.com>
2011-11-24 00:55:43 +0800

    Add URLPreview plugin for Jabbot.

---
+++ lib/Jabbot/Plugin/URLPreview.pm
@@ -0,0 +1,29 @@
+package Jabbot::Plugin::URLPreview;
+use warnings;
+use strict;
+use Jabbot::Plugin;
+use LWP::Simple;
+use Web::Query;
+
+sub can_answer {
+    my ($text) = @args;
+    return $text =~ m{https?://};  # match a url pattern ?
+}
+
+sub answer {
+    my ($text) = @args;
+    my ($url) = ($text =~ m{(https?://\S+)});
+
+    # XXX: do something with metacpan or search.cpan.org ?
+
+    my $title;
+    wq($url)->find('title')
+            ->each(sub {
+                my $i = shift;
+                $title = $_->text;
+            });
+    my $reply = sprintf '[ %s ]', $url;
+    return { content => $reply, confidence => 1 };
+}
+
+1;
