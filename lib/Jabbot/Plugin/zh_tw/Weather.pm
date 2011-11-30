03764738fe1a46f5a6f1d6aac8fe2ee4921ea7e4
Kang-min Liu <gugod@gugod.org>
2011-11-30 14:11:30 +0800

    more hint keywords for forecast

--- lib/Jabbot/Plugin/zh_tw/Weather.pm
+++ lib/Jabbot/Plugin/zh_tw/Weather.pm
@@ -13,7 +13,7 @@ sub can_answer {
 
     ($self->{area}) = $text =~ m/((?:南投|嘉義|基隆|宜蘭|屏東|彰化|新北|新竹|桃園|澎湖|臺中|臺北|臺南|臺東|台中|台北|台南|台東|花蓮|苗栗|連江|金門|雲林|高雄)(?:縣|市)?)/;
     ($self->{hint}) = $text =~ m/(weather|冷|暖|寒|暑|熱|涼|雨|晴|天氣|氣象)/;
-    ($self->{forcast}) = $text =~ m/(預報)/;
+    ($self->{forcast}) = $text =~ m/([明後]天|預報)/;
 
     return $self->{area} && $self->{hint};
 }
