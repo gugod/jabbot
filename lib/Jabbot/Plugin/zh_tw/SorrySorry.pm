f885d778604be242d55db38eaea432e3bed2a9b6
Kang-min Liu <gugod@gugod.org>
2012-03-15 15:51:09 +0800

    .

--- lib/Jabbot/Plugin/zh_tw/SorrySorry.pm
+++ lib/Jabbot/Plugin/zh_tw/SorrySorry.pm
@@ -9,6 +9,7 @@ sub can_answer {
     utf8::decode($text) unless utf8::is_utf8($text);
 
     my ($x) = $text =~ m{(
+                            對不起 | 道歉
                             !+ | ！+ |
                             可惡 | 混蛋 | 雜(碎｜種) | 幹你 |
                             靠(你|夭) |
