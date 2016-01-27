d94be7661a684cd94c170e37ace437700f7b2b87
Kang-min Liu <gugod@gugod.org>
2016-01-27 22:46:41 +0100

    rework the IRC frontend.
    Remove the use of AnyEvent::MP.

--- lib/Jabbot/Front/IRC.pm
+++ lib/Jabbot/Front/IRC.pm
@@ -7,10 +7,9 @@ use parent 'Jabbot::Component';
 use JSON qw(decode_json encode_json);
 use Encode qw(encode_utf8 decode_utf8);
 use Jabbot;
+use Jabbot::RemoteCore;
 
 use AnyEvent;
-use AnyEvent::MP;
-use AnyEvent::MP::Global;
 use AnyEvent::IRC::Client;
 
 sub init_irc_client {
@@ -56,26 +55,18 @@ sub init_irc_client {
             my ($client, $channel, $ircmsg) = @_;
             my $text = Encode::decode("utf8", $ircmsg->{params}[1]);
             my $nick = $client->nick;
-            my $to_me = $text =~ s/^${nick}:\s+//;
-            my $from_nick = AnyEvent::IRC::Util::prefix_nick($ircmsg->{prefix}) || "";
 
+            my $to_me = $text =~ s/^${nick}:\s+//;
+            return unless $to_me;
+
+            my $from_nick = AnyEvent::IRC::Util::prefix_nick($ircmsg->{prefix}) || "";
             return if $from_nick =~ /${nick}_*/;
 
-            my $ports = grp_get "jabbot-core" or return;
-
-            for (@$ports) {
-                snd $_, action => {
-                    name => 'answer',
-                    node => "jabbot-irc",
-                    args => {
-                        network  => $network->{name},
-                        channel  => $channel,
-                        from     => $from_nick,
-                        to_me    => $to_me,
-                        question => $text
-                    }
-                };
-            }
+            state $jabbot = Jabbot::RemoteCore->new();
+            my $answer = $jabbot->answer(q => $text);
+            my $reply_text = $answer->{body};
+            $reply_text = Encode::encode_utf8($reply_text);
+            $client->send_chan($channel, "PRIVMSG", $channel, "$from_nick: $reply_text");
         },
 
         error => sub {
@@ -88,10 +79,7 @@ sub init_irc_client {
     return $client;
 }
 
-my $AEMP_PORT_GUARD;
 sub run2 {
-    configure profile => "jabbot-irc";
-
     my $IRC_CLIENTS = {};
     my $networks = Jabbot->config->{irc}{networks};
     for (keys %$networks) {
@@ -99,52 +87,6 @@ sub run2 {
         $networks->{$_}{nick} ||= (Jabbot->config->{nick} || "jabbot_$$");
         $IRC_CLIENTS->{$_} = init_irc_client($networks->{$_})
     }
-
-    my @irc_privmsg_q;
-    my $irc_privmsg_t;
-
-    my $irc_send_privmsg  = sub {
-        return unless $IRC_CLIENTS->{$_[0]};
-
-        push @irc_privmsg_q, [@_];
-
-        $irc_privmsg_t ||= AE::timer 1, 1, sub {
-            my ($network, $channel, $body, $command) = @{shift @irc_privmsg_q};
-            $command ||= 'PRIVMSG';
-            $command = uc $command;
-
-            my $client  = $IRC_CLIENTS->{$network};
-            unless ($client->channel_list($channel)) {
-                $client->send_srv("JOIN", $channel);
-            }
-
-            $client->send_chan($channel, $command, $channel, $body);
-
-            undef $irc_privmsg_t unless @irc_privmsg_q;
-        };
-    };
-
-    my $port = rcv(
-        port,
-
-        post => sub {
-            my ($data, $reply_port) = @_;
-            $irc_send_privmsg->($data->{network}, $data->{channel}, encode_utf8($data->{body}) , $data->{command});
-        },
-
-        reply => sub {
-            my ($data) = @_;
-            return unless $data->{to_me} || $data->{answer}{confidence} == 1;
-
-            my $body = encode_utf8(
-                ($data->{to_me} ? ($data->{from} . ": ") : "") . $data->{answer}{content}
-            );
-
-            $irc_send_privmsg->($data->{network}, $data->{channel}, $body);
-        }
-    );
-
-    $AEMP_PORT_GUARD = grp_reg "jabbot-irc", $port;
 }
 
 sub run {
@@ -153,49 +95,6 @@ sub run {
             __PACKAGE__->run2
         }
     );
-}
-
-sub cat {
-    my ($class, $network, $channel, $command, $body) = @_;
-
-    if ($command && !$body) {
-        $body = $command;
-        $command = "PRIVMSG";
-    }
-
-    $body = Encode::decode_utf8($body) unless Encode::is_utf8($body);
-
-    $command = uc($command);
-
-    unless(grep { $command eq $_ } qw(PRIVMSG NOTICE)) {
-        $command = "PRIVMSG";
-    }
-
-    configure;
-
-    my $cv = AnyEvent->condvar;
-
-    my $w = AnyEvent->timer(
-        after => 1,
-        cb => sub {
-            my $irc = grp_get "jabbot-irc";
-            if ($irc) {
-                snd $_, post => {
-                    network => $network,
-                    channel => $channel,
-                    command => $command,
-                    body    => $body
-                } for @$irc;
-            }
-            else {
-                warn "No port found for jabbot-irc. Huh?";
-            }
-
-            $cv->send;
-        }
-    );
-
-    $cv->recv;
 }
 
 1;
