f3e6bdd323aaf08eb3195cd90900ea0b02bee0a9
Kang-min Liu <gugod@gugod.org>
2016-02-13 00:05:37 +0100

    randomly add some emotions

--- lib/Jabbot/Plugin/Echo.pm
+++ lib/Jabbot/Plugin/Echo.pm
@@ -1,6 +1,8 @@
 package Jabbot::Plugin::Echo;
 use v5.18;
-use Object::Tiny qw(core);
+use strict;
+use warnings;
+use Object::Tiny;
 
 sub can_answer {
     1;
@@ -8,6 +10,21 @@ sub can_answer {
 
 sub answer {
     my ($self, $text, $message) = @_;
+
+    my $chance = int(rand()*10);
+    my $emotion = {
+        8 => "...?",
+        7 => " XD",
+        6 => " !!",
+        5 => " :D",
+        4 => " :)",
+        3 => " :-P",
+        2 => " .... ???!",
+    }->{$chance} || "";
+
+    if ($emotion) {
+        $text = "$text $emotion";
+    }
 
     return {
         score => 0,
