97837dc526b1b58e428537521f8e38f8881c057f
Kang-min Liu <gugod@gugod.org>
2020-05-11 20:52:36 +0800

    Basically fix this PlurkPoster module.

--- lib/PlurkPoster.pm
+++ lib/PlurkPoster.pm
@@ -1,6 +1,5 @@
 package PlurkPoster;
-use v5.14;
-use Data::Dumper;
+use v5.26;
 use Mojo::UserAgent;
 
 sub new {
@@ -11,21 +10,22 @@ sub new {
 sub login {
     my ($self) = @_;
     $self->{ua} = Mojo::UserAgent->new;
-    $self->{ua}->on(
-        "start",
-        sub {
-            my ($ua, $tx) = @_;
-            my $headers = $tx->req->headers;
-            $headers->remove("Accept-Encoding");
-            $headers->header("User-Agent", "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543 Safari/419.3");
+
+    my $tx = $self->{ua}->get('https://www.plurk.com/login');
+    die "failed 1" if $tx->result->is_error;
+
+    my $login_token = $tx->result->dom->at("input[name=login_token]")->attr("value");
+
+    $tx = $self->{ua}->post(
+        'https://www.plurk.com/login',
+        form => {
+            nick_name   => $self->{username},
+            password    => $self->{password},
+            login_token => $login_token
         }
     );
 
-    my $tx = $self->{ua}->get('https://www.plurk.com/m/login');
-    die "failed 1" unless $tx->success;
-
-    $tx = $self->{ua}->post('https://www.plurk.com/m/login' => form => { username => $self->{username}, password => $self->{password} });
-    die "failed 2" unless $tx->success;
+    die "failed 2" if $tx->result->is_error;
 
     return $self;
 }
@@ -35,13 +35,13 @@ sub post {
     my $ua = $self->{ua};
 
     my @content;
-    if ( length($content) < 120 ) {
+    if ( length($content) < 360 ) {
         @content = ($content);
     }
     else {
-        my $pieces = int(length($content) / 120);
+        my $pieces = int(length($content) / 340);
         for (0 .. $pieces) {
-            push @content, substr($content, 120 * $_, 120);
+            push @content, substr($content, 340 * $_, 340);
         }
         for (1..$#content-1) {
             $content[$_] = "... $content[$_] ...";
@@ -53,43 +53,49 @@ sub post {
     my $text = shift @content;
     $text .= " $self->{hashtag}" if $self->{hashtag};
 
-    my $tx = $ua->get('http://www.plurk.com/m/');
-    die "failed 3" unless $tx->success;
+    my $tx = $ua->get('https://www.plurk.com/m');
+    my $res = $tx->result;
+    die "failed 3" if $res->is_error;
 
-    my $user_id = $tx->res->dom->find("input[name=user_id]")->first->attr("value");
+    my $user_id = $res->dom->at("input[name=user_id]")->attr("value");
+    my $language = $res->dom->at("input[name=language]")->attr("value");
+    my $form_token = $res->dom->at("input[name=form_token]")->attr("value");
 
-    $tx = $ua->post('http://www.plurk.com/m/' => form => {
+    $tx = $ua->post('https://www.plurk.com/m/' => form => {
         user_id => $user_id,
-        language => "en",
+        language => $language,
         qualifier => ":",
         content =>  $text,
+        form_token => $form_token,
     });
-    die "failed 4" unless $tx->success;
+    die "failed 4\n".$tx->result->body if $tx->result->is_error;
     say ">>> $text";
 
-    $tx = $ua->get('http://www.plurk.com/m/?mode=my');
-    die "failed 5" unless $tx->success;
+    $tx = $ua->get('https://www.plurk.com/m/?mode=my');
+    die "failed 5" if $tx->result->is_error;
 
-    my $link_to_plurk = $tx->res->dom->find("div.plurk[data-pid]")->first;
+    my $link_to_plurk = $tx->res->dom->at("div.plurk[data-pid]");
     my ($plurk_id) = $link_to_plurk->attr("data-pid");
-    my $plurk_permaurl = "http://www.plurk.com/m/p/${plurk_id}";
 
-    say "DEBUG: plurk = $plurk_permaurl";
-    while (@content) {
-        my $text = shift @content;
+    if (@content) {
+        my $plurk_permaurl = "http://www.plurk.com/m/p/${plurk_id}";
+        say "DEBUG: plurk = $plurk_permaurl";
+        while (@content) {
+            my $text = shift @content;
 
-        $tx = $ua->get($plurk_permaurl);
-        die "failed 5" unless $tx->success;
+            $tx = $ua->get($plurk_permaurl);
+            die "failed 5" if $tx->result->is_error;
 
-        say "RE> $text";
-        $tx = $ua->post("${plurk_permaurl}/#plurkbox" => form => {
-            user_id => $user_id,
-            language => "en",
-            qualifier => ":",
-            content => $text,
-        });
-        sleep 5;
-        die "failed 6" unless $tx->success;
+            say "RE> $text";
+            $tx = $ua->post("${plurk_permaurl}/#plurkbox" => form => {
+                user_id => $user_id,
+                language => "en",
+                qualifier => ":",
+                content => $text,
+            });
+            sleep 5;
+            die "failed 6" if $tx->result->is_error;
+        }
     }
 
     return $plurk_id;
