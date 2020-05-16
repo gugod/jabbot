0a6febe83d330285fa6127d890d2f5355608f592
Kang-min Liu <gugod@gugod.org>
2020-05-16 14:36:02 +0800

    split Kuso module by language code.

---
+++ lib/Jabbot/Plugin/en_us/Kuso.pm
@@ -0,0 +1,34 @@
+package Jabbot::Plugin::en_us::Kuso;
+use v5.18;
+use utf8;
+use Object::Tiny qw(core);
+
+sub can_answer {
+    my ($self, $message) = @_;
+    my $text = $message->{body};
+    my $ratio = length($text =~ s/\P{Latin}//r)/length($text);
+    return $ratio > 0.5 ? 1 : 0;
+}
+
+sub answer {
+    my ($self, $message) = @_;
+    my $text = $message->{body};
+
+    my $reply;
+
+    for($text) {
+        if(/^make\s+me\s?./i) {
+            $reply = "WHAT? MAKE IT YOURSELF"
+        }
+        elsif(/^sudo\s+make/) {
+            $reply = "OKAY"
+        }
+    }
+
+    return {
+        body => $reply,
+        score => 1
+    } if $reply;
+}
+
+1;
--- lib/Jabbot/Plugin/zh_tw/Kuso.pm
+++ lib/Jabbot/Plugin/zh_tw/Kuso.pm
@@ -3,7 +3,12 @@ use v5.18;
 use utf8;
 use Object::Tiny qw(core);
 
-sub can_answer { 1 }
+sub can_answer {
+    my ($self, $message) = @_;
+    my $text = $message->{body};
+    my $ratio = length($text =~ s/\P{Han}//r)/length($text);
+    return $ratio > 0.5 ? 1 : 0;
+}
 
 sub answer {
     my ($self, $message) = @_;
@@ -33,11 +38,11 @@ sub answer {
                 $self->{habiulai_count} = 0;
             }
         }
-        elsif(/^make\s+me\s?./i) {
-            $reply = "WHAT? MAKE IT YOURSELF"
+        elsif(/^幫我(\p{Han})/) {
+            $reply = "不要，你自己$1";
         }
-        elsif(/^sudo\s+make/) {
-            $reply = "OKAY"
+        elsif(/^請幫我/) {
+            $reply = "好喔";
         }
     }
 
