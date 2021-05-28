8998f47eecaf271f19d51297638ace9a9c801d34
Kang-min Liu <gugod@gugod.org>
2021-05-29 00:30:34 +0900

    report the top10 reservoir condition in a summary hbarchart.

--- lib/Jabbot/Fun.pm
+++ lib/Jabbot/Fun.pm
@@ -15,15 +15,14 @@ sub hbar {
     return $hbars[$b];
 }
 
-sub hbars_of_reservoir_usage_percentage {
+sub hbars_of_top10_reservoir_usage_percentage {
     my $d = Jabbot::External::TaiwanReservoir->new->usage_percentage;
 
     my %reservoir_by_name = map { $_->{"ReservoirName"} => $_ } grep { $_->{"ReservoirName"} } values %$d;
 
-    # List taken from https://water.taiwanstat.com/
-    my @names_north_to_south = qw(新山水庫 翡翠水庫 石門水庫 永和山水庫 寶山水庫 寶山第二水庫 明德水庫 鯉魚潭水庫 德基水庫 石岡壩 日月潭水庫 霧社水庫 湖山水庫 仁義潭水庫 蘭潭水庫 白河水庫 曾文水庫 烏山頭水庫 南化水庫 阿公店水庫 牡丹水庫);
+    my @top10_south_to_north = qw( 牡丹水庫 南化水庫 烏山頭水庫 曾文水庫 霧社水庫 日月潭水庫 鯉魚潭水庫 德基水庫 石門水庫 翡翠水庫 );
 
-    my @percentages = map { $reservoir_by_name{$_}{"UsagePercentage"} // 0 } reverse @names_north_to_south;
+    my @percentages = map { $reservoir_by_name{$_}{"UsagePercentage"} // 0 } @top10_south_to_north;
 
     return join "", map { hbar(int(100 * $_)) } @percentages;
 }
